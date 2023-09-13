5c62eb920f78e976f9f1b07c2f011e613027ac82
HelplessSoldier <patrick1234143211@gmail.com>
2023-09-13 16:00:35 -0700

    added default ship list

--- src/classes/game.js
+++ src/classes/game.js
@@ -1,6 +1,7 @@
 const Player = require("./player");
 const CpuPlayer = require("./cpuPlayer");
 const GameBoard = require("./gameBoard");
+const Ship = require("./ship");
 
 class Game {
   constructor(playerName, cpuName) {
@@ -8,6 +9,16 @@ class Game {
     this.cpu = new CpuPlayer(cpuName);
     this.playerBoard = new GameBoard(10, 10, "empty");
     this.cpuBoard = new GameBoard(10, 10, "empty");
+  }
+
+  getDefaultShipList() {
+    const carrier = new Ship(5, "Carrier", null, null);
+    const battleship = new Ship(4, "Battleship", null, null);
+    const destroyer = new Ship(3, "Destroyer", null, null);
+    const submarine = new Ship(3, "Submarine", null, null);
+    const patrol = new Ship(2, "Patrol", null, null);
+    const shipList = [carrier, battleship, destroyer, submarine, patrol];
+    return shipList;
   }
 }
 
